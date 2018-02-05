a62c64baccdf0e291d720de7397f1f2d43427913
Kevin Pimentel <kevin@gymautopilot.com>
2018-02-05 16:22:23 -0500

    Added first, last, and phone. Removed name.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,9 +15,11 @@ use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'first' 			=> $faker->firstName,
+        'last' 				=> $faker->lastName,
+        'email' 			=> $faker->unique()->safeEmail,
+        'phone' 			=> $faker->phoneNumber,
+        'password' 			=> bcrypt($faker->password),
+        'remember_token'	=> str_random(10),
     ];
 });
