6ccf3c0ce5ce75789ea7d638b569bad38b06a2f3
Kevin Pimentel <kevin@gymautopilot.com>
2018-02-05 16:20:34 -0500

    Constants initial commit.

---
+++ config/constants.php
@@ -0,0 +1,82 @@
+<?php
+
+return [
+        
+    'TASKLIST'  => [
+        'ARCHIVED'                  => 0,
+        'ACTIVE'                    => 1
+    ],
+    'MEALTYPES' => [			
+        'BREAKFAST'                 => 1,
+        'LUNCH'                     => 2,
+        'SNACK'                     => 3,
+        'DINNER'                    => 4,
+        'OTHER'                     => 5,
+    ],
+    'CATEGORIES' => [
+        'EDUCATION'                 => 0,
+        'FINANCIAL'                 => 1,
+        'FITNESS'                   => 2,
+        'PROFESSIONAL'              => 3,
+        'PHILANTHROPIC'             => 4,
+        'HEALTH'                    => 5,
+        'NUTRITION'                 => 6,
+        'CAREER'                    => 7,
+        'DEVELOPMENT'               => 8,
+        'COURSES'                   => 9,
+        'BOOKS'                     => 10,
+        'RESUME'                    => 11,
+        'CREDIT'                    => 12,
+        'COMPETITION'               => 13,        
+    ],
+    'RESULTS' => [
+        
+        // Running
+        
+        '1K'                            => '1K',
+        '5K'                            => '5K',
+        '8K'                            => '8K',
+        '10K'                           => '10K',
+        '15K'                           => '15K',
+        '1MILE'                         => '1 MILE',
+        '5MILE'                         => '5 MILE',
+        '25MILE'                        => '25 MILE',
+        'HALF'                          => 'HALF MARATHON',
+        'FULL'                          => 'MARATHON',
+        'OTHERRUNNING'                  => 'OTHERRUNNING',
+        
+        // Cycling
+        
+        'HCENTURY'                      => 'HALF CENTURY',
+        'MCENTURY'                      => 'METRIC CENTURY',
+        'CENTURY'                       => 'CENTURY',
+        'CRITERIUM'                     => 'CRITERIUM',
+        'OTHERCYCLING'                  => 'OTHER CYCLING',
+        
+        // Miscellaneous
+        
+        'OTHER'                         => 'OTHER',
+        
+    ],
+    'CHALLENGE' => [
+        'STEPS'                         => 0,
+    ],
+    'EVENTS' => [   
+        'RUNNING'                       => 1,
+        'CYCLING'                       => 2,        
+        'SWIMMING'                      => 3,        
+        'FAMILY'                        => 4,        
+        'PERSONAL'                      => 5,        
+        'ANNIVERSARY'                   => 6,        
+        'SWIMMING'                      => 7,
+        'DATE'                          => 8,
+        'LUNCH'                         => 9,
+        'WORKOUT'                       => 10,
+        'OTHER'                         => 11,
+    ],
+    'GOALSTATUS' => [
+        'OPEN'                          => 0,
+        'COMPLETED'                     => 1,
+        'ARCHIVED'                      => 2,
+    ]
+];
