674ef016aa20accd2930d6387f86fa73f94d1fc8
Kevin Pimentel <kevin@gymautopilot.com>
2018-02-16 22:48:29 -0500

    Added session status display to login view.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,9 +11,18 @@
 
 @section('content')
 <div class="container">
-    <div class="row"> 
-        <div class="col-md-6 col-md-offset-3">         
-            
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
+            @if (session('warning'))
+                <div class="alert alert-warning">
+                    {{ session('warning') }}
+                </div>
+            @endif
             <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
                 <legend>Login</legend>
                 {{ csrf_field() }}
