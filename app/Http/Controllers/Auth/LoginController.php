ee1ce6b73747989efb3c22e96e1db42cc2ce6de5
Kevin Pimentel <kevin@gymautopilot.com>
2018-02-16 22:58:42 -0500

    Implemented the authenticated method to validate user account activation.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -25,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/dashboard';
+    protected $redirectTo = '/tasklist';
 
     /**
      * Create a new controller instance.
@@ -35,5 +36,24 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * The user has been authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    public function authenticated(Request $request, $user)
+    {
+        if (!$user->verified) {
+            auth()->logout();
+            return back()->with(
+                'warning',
+                'You need to confirm your account. We have sent you an activation code, please check your email.'
+            );
+        }
+        return redirect()->intended($this->redirectPath());
     }
 }
